8392d1614f55abcb85a62150226ac9aa729e1606
ugrcyo <ugrcyo@github.com>
2020-01-31 00:34:40 +0300

    added 2D Infinite Bomber

---
+++ 2D Infinite Bomber/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField]float maxHp;
+    float currentHp;
+    [SerializeField] Image hpBar;
+    void Start()
+    {
+        currentHp = maxHp;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+        if (hpBar!=null)
+        {
+            UpdateHpUI(); 
+        }
+        if (currentHp<=0)
+        {
+            Die();
+        }
+    }
+    void UpdateHpUI()
+    {
+        hpBar.fillAmount = currentHp / maxHp;
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
