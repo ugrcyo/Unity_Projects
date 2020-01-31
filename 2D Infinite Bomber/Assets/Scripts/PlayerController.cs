4b569a0a63ac56139bb3db5004f44f20dcfb330d
ugrcyo <ugrcyo@github.com>
2020-02-01 01:55:45 +0300

    Update PlayerController.cs

--- 2D Infinite Bomber/Assets/Scripts/PlayerController.cs
+++ 2D Infinite Bomber/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rB2D; //Karakterimizin 2D uzayda fiziksel haraketi için
-    [SerializeField] float speed = 2; //Hız ayarlaması için 
-    Animator anim; //Karakterimizin animasyon kontrolü özelliğin için
+    Rigidbody2D rB2D; 
+    [SerializeField] float speed = 2;
+    Animator anim; 
     [SerializeField]Animator bloodFx;
     bool isFacingRight = true;
     [SerializeField] float jumpForce = 100;
@@ -27,20 +27,20 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         UpdateHealthUI();
-        rB2D = GetComponent<Rigidbody2D>(); //rB2D değişkeninin atamasını yapıyoruzs
-        anim = GetComponent<Animator>();    //anim değişkeninin atamasını yapıyoruz
+        rB2D = GetComponent<Rigidbody2D>(); 
+        anim = GetComponent<Animator>();    
     }
     private void Update()
     {
         fireTimer += Time.deltaTime;
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.LeftControl))//GetMouseButton mouse click için inputumuz 0 sol tık 1 sağ tık 2 ise orta tuşa denk gelmektedir 
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.LeftControl))
         {
             if (fireTimer>fireRate)
             {
                 fireTimer = 0;
                 if (isFacingRight)
                 {
-                    Instantiate(projectile, barrel.position, Quaternion.Euler(new Vector3(0, 0, -90)));//Instantiate verilen objeyi klonlar ve sahneye yerleştirir ilk parametre objeyi 2. parametre pozisyonunu 3. parametre ise açısını belirler Quaternion.Euler arayüzdeki gibi x,y,z değerleri olarak açı vermemizi sağlar
+                    Instantiate(projectile, barrel.position, Quaternion.Euler(new Vector3(0, 0, -90)))
                 }
                 else
                 {
@@ -61,27 +61,24 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        // Debug.Log(rB2D.velocity.y);
-        // Debug.LogWarning("<color=red>RAW: "+ Input.GetAxisRaw("Horizontal")+"</color>");//Debug yazdırılırken başına <color=renk> sonuna </color> eklenirse verilen renkte yazdırılır eğer Debug.Log sonuna Warning veya Error eklenirse yazı ikonu değiştirilir
-        //  Debug.LogError("<color=blue>Normal: " + Input.GetAxis("Horizontal") + "</color>");
-        Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rB2D.velocity.y); //input ile gidilecek birim yön vektörünü oluşturuyoruz.
+        Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rB2D.velocity.y); 
         if (!takingDamage)
         {
             rB2D.velocity = dir; 
-        }//Rigidbody2D nin hız değerini belirliyoruz
-        anim.SetBool("isWalking", 0 != Mathf.Abs(dir.x)); //Animasyon parametresi olan isWalking değerine input alınıp alınmadığını yazıyoruz (giriş değerinin mutlak değeri 1 mi)
+        }
+        anim.SetBool("isWalking", 0 != Mathf.Abs(dir.x)); 
         anim.SetFloat("velocityY", rB2D.velocity.y);
-        if (rB2D.velocity.x > 0 && !isFacingRight)//Karakterin gittiği yönde döndürmek için gittiği yönü ve şu anda dönük olduğu yönü kontrol ediyoruz
+        if (rB2D.velocity.x > 0 && !isFacingRight)
         {
-            isFacingRight = !isFacingRight;//Şu anda dönük olduğu yönü tutuyoruz
-            Reverse();//Karakterin scale ini - ile çarparak ters çeviriyoruz
+            isFacingRight = !isFacingRight;
+            Reverse();
         }
         else if (rB2D.velocity.x < 0 && isFacingRight)
         {
             isFacingRight = !isFacingRight;
             Reverse();
         }
-        if (/*Input.GetAxis("Jump")==1||*/Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             rB2D.AddForce(new Vector2(0, jumpForce));
             isGrounded = false;
@@ -102,7 +99,6 @@ public class PlayerController : MonoBehaviour
         Vector3 charScale = transform.localScale;
         charScale.x *= -1;
         transform.localScale = charScale;
-        //   transform.localScale = new Vector3(transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
